11ed7775fae608ac964a8510c30db32dc7385bb8
John M. Drescher <drescherjm@gmail.com>
2019-09-20 23:50:58 -0400

    Update portfile.cmake
    qxt now correctly installs the core module for windows!

--- ports/qxt/portfile.cmake
+++ ports/qxt/portfile.cmake
@@ -31,21 +31,13 @@ vcpkg_configure_qmake(
 )
 
 file(APPEND "${DEBUG_DIR}/.qmake.cache" "QXT_BUILD_TREE=${DEBUG_DIR} \nQXT_INSTALL_BINS=${DEBUG_DIR}/bin \nQXT_INSTALL_HEADERS=${DEBUG_DIR}/include \n")
-file(APPEND "${DEBUG_DIR}/.qmake.cache" QXT_INSTALL_FEATURES=${DEBUG_DIR}/features\n")
+file(APPEND "${DEBUG_DIR}/.qmake.cache" "QXT_INSTALL_FEATURES=${DEBUG_DIR}/features\n")
 
 file(APPEND "${RELEASE_DIR}/.qmake.cache" "QXT_BUILD_TREE=${RELEASE_DIR} \nQXT_INSTALL_BINS=${RELEASE_DIR}/bin \nQXT_INSTALL_HEADERS=${RELEASE_DIR}/include \n")
-file(APPEND "${RELEASE_DIR}/.qmake.cache" QXT_INSTALL_FEATURES=${RELEASE_DIR}/features\n")
+file(APPEND "${RELEASE_DIR}/.qmake.cache" "QXT_INSTALL_FEATURES=${RELEASE_DIR}/features\n")
 
 set(ENV{QXT_BUILD_TREE} ${CURRENT_BUILDTREES_DIR})
 
-
-# file(GLOB_RECURSE RELEASE_MAKEFILES ${RELEASE_DIR}/*Makefile*)
-
-#Set the correct install directory to packages
-# foreach(MAKEFILE ${RELEASE_MAKEFILES} ${DEBUG_MAKEFILES})
-    # message(STATUS MAKEFILE=${MAKEFILE})
-    # vcpkg_replace_string(${MAKEFILE} "(INSTALL_ROOT)" "(INSTALL_ROOT)${PACKAGES_DIR_WITHOUT_DRIVE}")
-# endforeach()
 
 if (CMAKE_HOST_WIN32)
     vcpkg_build_qmake(
@@ -59,45 +51,19 @@ elseif (CMAKE_HOST_UNIX OR CMAKE_HOST_APPLE) # Build in UNIX
     )
 endif()
 
-# vcpkg_configure_qmake(
-    # SOURCE_PATH ${SOURCE_PATH}
-    # OPTIONS
-        # CONFIG+=${VCPKG_LIBRARY_LINKAGE}
-		# QXT_MODULES=widgets
-# )
-
-# if (CMAKE_HOST_WIN32)
-    # vcpkg_build_qmake(
-        # RELEASE_TARGETS sub-src-widgets
-        # DEBUG_TARGETS sub-src-widgets
-    # )
-# elseif (CMAKE_HOST_UNIX OR CMAKE_HOST_APPLE) # Build in UNIX
-    # vcpkg_build_qmake(
-        # RELEASE_TARGETS sub-tools-3rdparty-qdoc3 sub-src-core sub-src-widgets sub-src-sql
-        # DEBUG_TARGETS sub-tools-3rdparty-qdoc3 sub-src-core sub-src-widgets sub-src-sql
-    # )
-# endif()
-
-# file(GLOB_RECURSE RELEASE_MAKEFILES ${RELEASE_DIR}/*Makefile*)
-
-#Set the correct install directory to packages
-# foreach(MAKEFILE ${RELEASE_MAKEFILES} ${DEBUG_MAKEFILES})
-    # message(STATUS MAKEFILE=${MAKEFILE})
-    # vcpkg_replace_string(${MAKEFILE} "(INSTALL_ROOT)" "(INSTALL_ROOT)${PACKAGES_DIR_WITHOUT_DRIVE}")
-# endforeach()
 
 #Install the header files
-file(GLOB HEADER_FILES ${SOURCE_PATH}/src/*.h)
-file(INSTALL ${HEADER_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include/qxt)
+#file(GLOB HEADER_FILES ${RELEASE_DIR}/include/*.h)
+file(INSTALL ${RELEASE_DIR}/include DESTINATION ${CURRENT_PACKAGES_DIR}/include/qxt)
 
 #Install the module files
 if (CMAKE_HOST_WIN32)
-    file(INSTALL ${CURRENT_BUILDTREES_DIR}/lib/QxtCore.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-    file(INSTALL ${CURRENT_BUILDTREES_DIR}/lib/QxtCored.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+    file(INSTALL ${RELEASE_DIR}/lib/QxtCore.lib ${RELEASE_DIR}/lib/QxtCore.exp DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+    file(INSTALL ${DEBUG_DIR}/lib/QxtCored.lib ${DEBUG_DIR}/lib/QxtCored.exp DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
 
     if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-        file(INSTALL ${CURRENT_BUILDTREES_DIR}/lib/QxtCore.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-        file(INSTALL ${CURRENT_BUILDTREES_DIR}/lib/QxtCored.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+        file(INSTALL ${RELEASE_DIR}/bin  DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+        file(INSTALL ${DEBUG_DIR}/bin DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
     endif()
     vcpkg_copy_pdbs()
 elseif (CMAKE_HOST_UNIX OR CMAKE_HOST_APPLE) # Build in UNIX
