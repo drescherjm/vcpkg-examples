5d15388728cbf625aa53de070354712ad67e6a54
John M. Drescher <drescherjm@gmail.com>
2019-11-01 22:07:29 -0400

    Add image parameters

--- Examples/cli11/CLI_0/src/cli_0.cxx
+++ Examples/cli11/CLI_0/src/cli_0.cxx
@@ -7,11 +7,17 @@ int main(int argc, char **argv) {
     // Add new options/flags here
 	bool my_flag = true;
 	app.add_flag("--flag,!--no-flag", my_flag, "Optional description");
+
+	std::vector<std::string> imageFiles;
+
+	app.add_option("--image", imageFiles, "Image Files")->check(CLI::ExistingFile);
 	
     CLI11_PARSE(app, argc, argv);
 
 	std::boolalpha(std::cout);
 	std::cout << my_flag;
+
+	std::cout << app.config_to_str(true, true) << std::endl;
 
     return 0;
 }
