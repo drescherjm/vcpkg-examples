b66a65f52a8c2276894a9cc14f47ff12261048ce
John M. Drescher <jdrescher@RADIMGWS130.RADIMG>
2019-11-08 13:49:17 -0500

    Begin to add TIFF example

---
+++ Examples/vtk/IO/ReadTIFF/src/ReadTIFF.cxx
@@ -0,0 +1,38 @@
+#include <vtkSmartPointer.h>
+#include <vtkImageViewer2.h>
+#include <vtkTIFFReader.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+
+int main(int argc, char* argv[])
+{
+  //Verify input arguments
+  if ( argc != 2 )
+  {
+    std::cout << "Usage: " << argv[0]
+              << " Filename(.tif) e.g. ColorCells.tif" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  //Read the image
+  vtkSmartPointer<vtkTIFFReader> reader =  vtkSmartPointer<vtkTIFFReader>::New();
+  reader->SetFileName ( argv[1] );
+
+  // Visualize
+  vtkSmartPointer<vtkImageViewer2> imageViewer =
+    vtkSmartPointer<vtkImageViewer2>::New();
+  imageViewer->SetInputConnection(reader->GetOutputPort());
+  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+
+  imageViewer->SetColorLevel(32000);
+  imageViewer->SetupInteractor(renderWindowInteractor);
+  imageViewer->Render();
+  imageViewer->GetRenderer()->ResetCamera();
+  imageViewer->Render();
+
+  renderWindowInteractor->Start();
+
+  return EXIT_SUCCESS;
+}
