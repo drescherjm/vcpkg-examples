e8253c7fbefa65af1d9fce82a1359520dc626590
John M. Drescher <drescherjm@gmail.com>
2020-11-22 15:08:28 -0500

    Add the dark mode from https://github.com/ColinDuquesnoy/QDarkStyleSheet

--- Examples/Qt/Other/DarkMode0/src/main.cxx
+++ Examples/Qt/Other/DarkMode0/src/main.cxx
@@ -1,12 +1,33 @@
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
 #include "DM0Dialog.h"
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+void setupDarkMode()
+{
+	QFile f(":qdarkstyle/style.qss");
+
+	if (!f.exists()) {
+		printf("Unable to set stylesheet, file not found\n");
+	}
+	else {
+		f.open(QFile::ReadOnly | QFile::Text);
+		QTextStream ts(&f);
+		qApp->setStyleSheet(ts.readAll());
+	}
+}
+
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char *argv[])
 {
-
 	QApplication app(argc, argv);
+
+	setupDarkMode();
 
 	DM0Dialog mainWindow;
 	mainWindow.setGeometry(100, 100, 700, 700);
@@ -14,3 +35,5 @@ int main(int argc, char *argv[])
 
     return app.exec();
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////
